8918666230e2dad17f5767c4a3ad3a3535c6021e
Christopher <christophermoeller2000@gmail.com>
2025-06-11 15:10:12 +0200

    oops leaked a secret key

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Also an npm install
 
 
 Create a .env file with `API_IDENTITY`, `LATITUDE`, `LONGITUDE`, AND ~`DMI_API_KEY`~
+
 You must set an API_IDENTITY equal to a valid user agent in accordance to YR.no ToS: https://developer.yr.no/doc/TermsOfService/
+
 Set LATITUDE/LONGITUDE equal to what you want, in accordance to YR.no ToS (truncate coordinate decimals to 4 places): https://developer.yr.no/doc/TermsOfService/
 
 In order to use DMI data, you must create an account and generate an API KEY. Their email provider is down and their API is unstable, so currently this feature is not implemented
--- fetchWeather_DMI.js
+++ fetchWeather_DMI.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch');
 
 // const LATITUDE = 0.00
 // const LONGITUDE = 0.00
+const DMI_API_KEY = process.env.DMI_API_KEY;
 
-var weatherFetch =   "https://dmigw.govcloud.dk/v1/forecastedr/collections/harmonie_dini_sf/position?coords=POINT%2812.561%2055.715%29&crs=crs84&parameter-name=temperature-0m&f=GeoJSON&api-key=4b9e4320-773a-44ab-bf08-18bfe3a2ddfb"
+var weatherFetch =   "https://dmigw.govcloud.dk/v1/forecastedr/collections/harmonie_dini_sf/position?coords=POINT%2812.561%2055.715%29&crs=crs84&parameter-name=temperature-0m&f=GeoJSON&api-key=DMI_API_KEY"
 fetch(weatherFetch)
   .then(response => {
     console.log(response)
